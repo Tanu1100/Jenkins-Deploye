58d036087e75ce81be35dfd126643b59f70c8514
Tanu1100 <94030366+Tanu1100@users.noreply.github.com>
2021-11-27 23:35:14 +0530

    Update SecureController.java
    changes done

--- InMemorySecurity/src/main/java/com/secure/controller/SecureController.java
+++ InMemorySecurity/src/main/java/com/secure/controller/SecureController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecureController {
 	
 	
-	@GetMapping("/h")
+	@GetMapping("/hi")
 	public String Home()
 	{
 		return "Home Page:: Welcome to Home Page";
